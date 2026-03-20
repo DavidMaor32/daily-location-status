ddcebf94e96ea98928589170897977fcb00c2432
DavidMaor32 <126754136+DavidMaor32@users.noreply.github.com>
2026-03-20 17:53:18 +0200

    make the DATABASE_URL pass explicit

--- backend/src/services/database.ts
+++ backend/src/services/database.ts
@@ -1,8 +1,13 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import z from "zod";
-
-export const prisma = new PrismaClient();
 
 export const DatabaseConfigSchema = z.object({
     DATABASE_URL: z.url()
 });
+export type DatabaseConfig = z.infer<typeof DatabaseConfigSchema>;
+
+export const createDBClient = (config: DatabaseConfig) => 
+    new PrismaClient({adapter:
+        new PrismaPg({ connectionString: config.DATABASE_URL})
+    });
