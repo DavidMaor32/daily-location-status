b904bbec42514365ef1e24c9032c62296f151afc
DavidMaor32 <126754136+DavidMaor32@users.noreply.github.com>
2026-03-20 10:38:23 +0200

    imp. user router

---
+++ backend/src/User/router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { UserDal } from "./dal";
+import * as handlers from "./handlers";
+import { httpLogger } from "../utils/decorators";
+
+export const createUserRouter = (dal: UserDal) => {
+  const router = Router();
+  const decoratedHandlers = createDecoratedUserHandlers(dal);
+
+  router.get("/", decoratedHandlers.getAllUsersHandler);
+  router.get("/:id", decoratedHandlers.getUserById);
+  router.put("/:id", decoratedHandlers.updateUser);
+
+  return router;
+};
+
+export const createDecoratedUserHandlers = (dal: UserDal) => ({
+  getAllUsersHandler: httpLogger(
+    handlers.getAllUsersHandler(dal),
+    "getAllUsersHandler",
+  ),
+  getUserById: httpLogger(handlers.getUserByIdHandler(dal), "getUserById"),
+  updateUser: httpLogger(handlers.updateUser(dal), "updateUser"),
+});
